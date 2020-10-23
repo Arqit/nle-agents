6d37d6a4b52429b547a6f9deb4213840bbdee961
Mayur28 <mayur.28.ranchod@gmail.com>
2020-10-23 20:29:56 +0200

    Update MyAgent.py

--- src/MyAgent.py
+++ src/MyAgent.py
@@ -1,221 +1,102 @@
 from AbstractAgent import AbstractAgent
+from gym import spaces
+from replay_buffer import ReplayBuffer
 import torch.nn as nn
 import torch
 import numpy as np
+from torchsummary import summary
 
-
-# Read the DQN paper
-# This will then help when we do the DQN approach (hopefully friday)
-# We'll also need to include the ResetEnvironment in ours to be able to train successively for many rounds (should pay attention to how we maintain state between episodes where necessary)
 # We'll probably take some inspiration from their train function
-
-
-# def Crop(the world, agents position, the radius around the agent that we want to crop)
-# Think maybe via index slicing
-
-# Just find a way to augment the stuff nicely
+# The architecture will probably require some experimentation
 
 class DQN(nn.Module):
     """
-    Insert description here
+    A basic implementation of a Deep Q-Network. The architecture is the same as that described in the
+    Nature DQN paper.
     """
-
-    def __init__(self,observation,num_actions,embedding_dim=32,crop_dim=9,num_layers=5):
-        super.__init__()
-
-        self.map_size = observation['glyph'].shape
-        self.blstats_size = observation['blstats'].shape[0]
-        # glpyhs, colors and chars all have the same size...
-        self.num_actions num_actions
-        self.H = self.map_size[0]
-        self.W = self.map_size[1]
-
-        self.embed_dim = embedding_dim # Check if we are working with embedding
-        self.h_size = 512 # This is the width of the network (number of nodes in each layer)
-
-
-        self.crop_dim = crop_dim
-
-        self.crop = Crop(self.H, self.W, self.crop_dim)
-        self.embed = nn.Embedding(nethack.MAX_GLYPHS,self.embed_dim) # This needs to be trained!
-        # Each word (glyph in our case) will be represented by a vector embedding with size vector embedding
+# Just check if there's any notable difference in whether a box or nd array is used?
+    def __init__(self, observation_space, action_space: spaces.Discrete):
         """
-        Embedding: just creates a Lookup Table, to get the word embedding given a word index.
-        I get the feeling that this is something like a mapper /translator
+        Initialise the DQN
+        :param observation_space: the state space of the environment
+        :param action_space: the action space of the environment
         """
+        super().__init__()
 
-        # Which network is this for?
-        # Check if all 3 input subnetworks have the same embedding dimension)
-        # These are the hyperparameters of the network
-        K = embedding_dim  # number of input filters
-        F = 3  # filter dimensions
-        S = 1  # stride
-        P = 1  # padding
-        M = 16  # number of intermediate filters
-        Y = 8  # number of output filters
-        L = num_layers  # number of convnet layers
-
-        in_channels = [K] + [M] * (L - 1)  # [K, M,M,M,M,M,M for L-1 times]--> Handle this better
-        out_channels = [M] * (L - 1) + [Y]
-
-        def interleave(xs,ys): #???? What is going on here
-            return [val for pair in zip(xs, ys) for val in pair]
-
-        # We now define the structure of the network ( which one - there are many networks that we are dealing with)
-        conv_extract = [
-            nn.Conv2d(
-                in_channels=in_channels[i],
-                out_channels=out_channels[i],
-                kernel_size=(F, F),
-                stride=S,
-                padding=P,
-            )
-            for i in range(L)
-        ]
-
-        self.extract_representation = nn.Sequential(
-            *interleave(conv_extract, [nn.ELU()] * len(conv_extract))
-        )
-
-        # CNN crop model. --> Structure is identical to the previously defined model
-        # This is the network that deals with the world
-        conv_extract_crop = [
-            nn.Conv2d(
-                in_channels=in_channels[i],
-                out_channels=out_channels[i],
-                kernel_size=(F, F),
-                stride=S,
-                padding=P,
-            )
-            for i in range(L)
-        ]
+        # Below is basically directly copied over from our DQN agent. Will need to be adjusted since it expects input with size (4,84,84)
+        input_shape = observation_space.shape
+        self.conv = nn.Sequential(
+        nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+        nn.ReLU(),
+        nn.Conv2d(32, 64, kernel_size=4, stride=2),
+        nn.ReLU(),
+        nn.Conv2d(64, 64, kernel_size=3, stride=1),
+        nn.ReLU())
 
 
-        # This interleave function seems to be extremely important
-        self.extract_crop_representation = nn.Sequential(
-            *interleave(conv_extract_crop, [nn.ELU()] * len(conv_extract))
-        ) # Unpacks the values (removes the commas and the square brackets)
+        #print(self.conv)
 
-
-        # WE DONT NEED THIS BUT REGARDLESS, SHOULD UNDERSTAND WHAT ARE THEY TRYING TO DO
-        out_dim = self.k_dim
-        # CNN over full glyph map
-        out_dim += self.H * self.W * Y
-
-        # CNN crop model.
-        out_dim += self.crop_dim ** 2 * Y
-
-        # Network to embed the blstats... what exactly does this mean?
-        self.embed_blstats = nn.Sequential(
-            nn.Linear(self.blstats_size, self.k_dim),
-            nn.ReLU(),
-            nn.Linear(self.k_dim, self.k_dim),
-            nn.ReLU(),
-        )
-
-        # Look into what is this used for? -> Check if we will be needing this
+        conv_out_size = self._get_conv_out(input_shape)
         self.fc = nn.Sequential(
-            nn.Linear(out_dim, self.h_dim),
-            nn.ReLU(),
-            nn.Linear(self.h_dim, self.h_dim),
-            nn.ReLU(),
-        )
+        nn.Linear(conv_out_size, 512),
+        nn.ReLU(),
+        nn.Linear(512, action_space.n))
+
+    def _get_conv_out(self, shape):
+        o = self.conv(torch.zeros(1, *shape))
+        return int(np.prod(o.size()))
 
 
-    def forward(self, env_outputs, core_state):
-        # Time seems to be an important aspect (not sure if this is solely related to the LSTM?)
-        # -- [T x B x H x W]
-        glyphs = env_outputs["glyphs"]
+    def forward(self, x):
+        conv_out = self.conv(x).view(x.size()[0], -1)
+        return self.fc(conv_out)
 
-        # -- [T x B x F]
-        blstats = env_outputs["blstats"]
-        print(glyphs.shape)
 
-        T, B, *_ = glyphs.shape
 
-        # -- [B' x H x W]
-        glyphs = torch.flatten(glyphs, 0, 1)  # Merge time and batch.
-
-        # -- [B' x F]
-        blstats = blstats.view(T * B, -1).float()
-
-        # -- [B x H x W]
-        glyphs = glyphs.long()
-
-        # Check what exactly is blstats stored as, to understand the way that they are indexing
-        # -- [B x 2] x,y coordinates
-        coordinates = blstats[:, :2]
-        blstats = blstats.view(T * B, -1).float()
-        # -- [B x K]
-        blstats_emb = self.embed_blstats(blstats)
-
-        assert blstats_emb.shape[0] == T * B
-
-        reps = [blstats_emb]
-
-        # -- [B x H' x W']
-        crop = self.crop(glyphs, coordinates)
-
-        # print("crop", crop)
-        # print("at_xy", glyphs[:, coordinates[:, 1].long(), coordinates[:, 0].long()])
-
-        # -- [B x H' x W' x K]
-        crop_emb = self._select(self.embed, crop)
-
-        # CNN crop model.
-        # -- [B x K x W' x H']
-        crop_emb = crop_emb.transpose(1, 3)  # -- TODO: slow?
-        # -- [B x W' x H' x K]
-        crop_rep = self.extract_crop_representation(crop_emb)
-
-        # -- [B x K']
-        crop_rep = crop_rep.view(T * B, -1)
-        assert crop_rep.shape[0] == T * B
-
-        reps.append(crop_rep) # This is how we are collecting the different representations, this is what we need
-
-        # -- [B x H x W x K]
-        glyphs_emb = self._select(self.embed, glyphs)
-        # glyphs_emb = self.embed(glyphs)
-        # -- [B x K x W x H]
-        glyphs_emb = glyphs_emb.transpose(1, 3)  # -- TODO: slow?
-        # -- [B x W x H x K]
-        glyphs_rep = self.extract_representation(glyphs_emb)
-
-        # -- [B x K']
-        glyphs_rep = glyphs_rep.view(T * B, -1)
-
-        assert glyphs_rep.shape[0] == T * B
-
-        # -- [B x K'']
-        reps.append(glyphs_rep)
-        # Now, reps contains the blstats rep, the crop rep and now the glyph_rep
-
-        st = torch.cat(reps, dim=1)
-
-        # -- [B x K]
-        st = self.fc(st)# --> Check if we will be needing the fc
-
+class MyAgent:
+    def __init__(
+        self,
+        observation_space: spaces.Box,
+        action_space: spaces.Discrete,
+        replay_buffer: ReplayBuffer,
+        use_double_dqn,
+        lr,
+        batch_size,
+        gamma,
+    ):
         """
-
-
-
-
-class MyAgent(AbstractAgent):
-    def __init__(self, observation_space, action_space,replay_buffer, lr, batch_size, gamma):
+        Initialise the DQN algorithm using the Adam optimiser
+        :param action_space: the action space of the environment
+        :param observation_space: the state space of the environment
+        :param replay_buffer: storage for experience replay
+        :param lr: the learning rate for Adam
+        :param batch_size: the batch size
+        :param gamma: the discount factor
+        """
         self.observation_space = observation_space
         self.action_space = action_space
-        self.replay_buffer = replay_buffer
+        self.replay_buffer = replay_buffer #maybe the wrong replay buffer is being used
+        self.use_double_dqn = use_double_dqn
         self.lr = lr
         self.batch_size = batch_size
         self.gamma = gamma
-        self.Q= DQN(self.observation_space,self.action_space)
+        # TODO: Initialise -agent's networks-, optimiser and -replay buffer-
+        self.Q = DQN(self.observation_space,self.action_space)
         self.Q.cuda()
-        self.Q_hat = DQN(self.observation_space,self.action_space)
+        self.Q_hat = DQN(self.observation_space, self.action_space)
         self.Q_hat.cuda()
-        self.optimizer = torch.optim.Adam(self.Q.parameters(),lr=self.lr)
+        self.optimizer = torch.optim.Adam(self.Q.parameters(), lr=self.lr)
 
-    def optimize_td_loss(self):
+    def optimise_td_loss(self):
+        """
+        Optimise the TD-error over a single minibatch of transitions
+        :return: the loss
+        """
+        # TODO
+        #   Optimise the TD-error over a single minibatch of transitions
+        #   Sample the minibatch from the replay-memory
+        #   using done (as a float) instead of if statement
+        #   return loss
         samples = self.replay_buffer.sample(self.batch_size)
         states = torch.tensor(samples[0]).type(torch.cuda.FloatTensor)
         next_states = torch.tensor(samples[3]).type(torch.cuda.FloatTensor)
@@ -223,7 +104,7 @@ class MyAgent(AbstractAgent):
         rewards = torch.tensor(samples[2]).type(torch.cuda.FloatTensor)
         done = torch.tensor(samples[4]).type(torch.cuda.LongTensor)
 
-        actual_Q = self.Q(states).gather(1,actions.unsqueeze(-1)).squeeze(-1)
+        actual_Q = self.Q(states).gather(1, actions.unsqueeze(-1)).squeeze(-1)# This is correct!!! You can google this if you want... the gather is needed so that we can index the self.Q(state_V) using a tensor
         Q_primes = self.Q_hat(next_states).max(1)[0]
         Q_primes[done] = 0.0
         Q_primes = Q_primes.detach()
@@ -235,9 +116,6 @@ class MyAgent(AbstractAgent):
         self.optimizer.step()
 
         return loss_t.item()
-        # for example, if your agent had a Pytorch model it must be load here
-        # model.load_state_dict(torch.load( 'path_to_network_model_file', map_location=torch.device(device)))
-
 
     def update_target_network(self):
         """
@@ -254,11 +132,13 @@ class MyAgent(AbstractAgent):
     def load_network_weights(self):
         self.Q.load_state_dict(torch.load("The_weights.pth"))
 
-    def act(self, observation):
-        # Perform processing to observation
-
+    def act(self, state: torch.Tensor):  # Correct
+        """
+        Select an action greedily from the Q-network given the state
+        :param state: the current state
+        :return: the action to take
+        """
         # TODO Select action greedily from the Q-network given the state
-        the_state = torch.unsqueeze(torch.from_numpy(np.array(state)), 0)
-        the_state = the_state.type(torch.cuda.FloatTensor)
+        the_state = state.type(torch.cuda.FloatTensor)
         the_answer = self.Q.forward(the_state).cpu()
         return torch.argmax(the_answer)
