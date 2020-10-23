e897292cb3711bc03e9eb0e6d4014d08271940ca
Mayur28 <mayur.28.ranchod@gmail.com>
2020-10-23 22:51:58 +0200

    Update evaluation.py

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,4 +1,3 @@
-import numpy as np
 import gym
 import nle
 import random
@@ -10,10 +9,13 @@ import random
 from torchsummary import summary
 
 np.set_printoptions(threshold=np.inf,linewidth=100000)
-# Eventually, look into clipping the rewards (The justifacation being stated in the DeepMind DQN paper)
-# use their Resetting environment to be trained indefinitely... Otherwise, it will terminate after every episode --> But this is a normal gym environment
 # We're going to have to use other things as input as well because our "local view" around the agent is usually small and typically, neural networks would need larger inputs than that to be useful
-#
+# Find a way to get it to work on colab to save time and sanity!
+# We still have to food we choose to eat? I saw that there's still a message: What do you want to eat? [fghi or ?*]
+# All cases need to be handled properly... It gets stuck if it chooses to eat but doesnt select proper food
+# For some reason, the network is producing the same answer (kinda makes sense because if the network is produced with the same input, it will produce the same answer)... Obviously, if the action taken is futile, we get the same state
+
+# Look into ways of sampling from the experience buffer more efficently
 
 def run_episode(env):
     # create instance of MyAgent
@@ -35,15 +37,15 @@ def Crop(observation, stacked_world, radius = 5):
     half_radius = np.int(radius/2)
     stats = observation['blstats']
     x = stats[0]
-    y = stats[1]
-    return stacked_world[:,y-half_radius:y+half_radius+1,x-half_radius:x+half_radius+1]
+    y = stats[1] # I'm adding the 29 because I'm now passing the padded world which pads above and below... To cut-pass the top padding, I have to add 29 to the row
+    return stacked_world[:,29+ y-half_radius:29+ y+half_radius+1, (x-half_radius):x+half_radius+1] # Fix this so that we pad to the correct size when we close to the edge!
 
 
 if __name__ == '__main__':
     # Seed
     seeds = [1,2,3,4,5]
 
-    hyper_params = {
+    hyper_params = { # Tinker around with these
         "seed": 42,  # which seed to use
         "replay-buffer-size": int(10000),  # replay buffer size
         "learning-rate": 1e-4,  # learning rate for Adam optimizer
@@ -77,6 +79,9 @@ if __name__ == '__main__':
     # I've read through the Pong wrappers and I dont think we need any of them
     # Im starting with their naive epsilon strategy, will change later to be fancy
 
+    # I dont think the agent knows what is it's position? -> Look into how is this handled (check also how was this done for Pong)
+
+    print(env.__dict__)
     test= env.reset()
     my_x = test['blstats'][0]
     my_y = test['blstats'][1]
@@ -84,7 +89,7 @@ if __name__ == '__main__':
     env.seed(hyper_params["seed"])
     # Remember that the size of the world is 21 x79
     #print(type(test["glyphs"]))
-    # print(test["glyphs"])
+    #print(test["glyphs"])
     #print(test['chars'])
 
 
@@ -93,11 +98,12 @@ if __name__ == '__main__':
     # Below stacks the glyph, colors and chars to form a map... will have the depth channel first
     stacked_version = torch.cat((torch.cat((torch.unsqueeze(torch.from_numpy(test['glyphs']),0),torch.unsqueeze(torch.from_numpy(test['colors']),0))),torch.unsqueeze(torch.from_numpy(test['chars']),0)))
     #print(stacked_version.shape)
-    output = Crop(test,stacked_version,hyper_params["crop_dim"]) # One thing that is quite important and you could look into is, if our agent is close to a corner, the cropping function wont return the correct size... To fix this issue, we can pad each "slice of the map" with the
                                         # appropriate placeholder (0 for example in the case of glyphs, 32 in chars, etc)
     # For whatever it's worth, I'm creating a padded version of the world to size 79x79 incase we decide to also pass the whole world as input
     padded_world = np.zeros((3,79,79))
     padded_world[:,29:50,:] = stacked_version
+    output = Crop(test,padded_world,hyper_params["crop_dim"]) # One thing that is quite important and you could look into is, if our agent is close to a corner, the cropping function wont return the correct size... To fix this issue, we can pad each "slice of the map" with the
+
 
 
     """
@@ -109,6 +115,7 @@ if __name__ == '__main__':
             break
     """
 
+    # I currently have it where it accepts the world as input
     replay_buffer = ReplayBuffer(hyper_params["replay-buffer-size"])
     agent = MyAgent(np.zeros((3,79,79)), env.action_space, replay_buffer, hyper_params["use-double-dqn"],
                     hyper_params["learning-rate"], hyper_params["batch-size"], hyper_params["discount-factor"])
@@ -126,22 +133,16 @@ if __name__ == '__main__':
 
 
     for t in range(hyper_params["num-steps"]):
-        fraction = min(1.0, float(t) / eps_timesteps)
-        eps_threshold = hyper_params["eps-start"] + fraction * (
-                hyper_params["eps-end"] - hyper_params["eps-start"]
-        )
-
         stacked_version = torch.cat((torch.cat((torch.unsqueeze(torch.from_numpy(state['glyphs']),0),torch.unsqueeze(torch.from_numpy(state['colors']),0))),torch.unsqueeze(torch.from_numpy(state['chars']),0)))
-        output = Crop(state,stacked_version,hyper_params["crop_dim"])
         padded_world[:,29:50,:] = stacked_version
+        output = Crop(state,padded_world,hyper_params["crop_dim"])
         padded_world = torch.tensor(padded_world)
 
-        sample = random.random()
-        if sample < eps_threshold:
+        if state["blstats"][10]/state["blstats"][11] > 0.7: # Experiment with this parameter... I currently have it where epsilon is dependent on the health status (HP= HitPoints)
             action = np.random.choice(agent.action_space.n)
         else:
-            #print(torch.squeeze(padded_world,0))
-            action = agent.act(torch.unsqueeze(padded_world,0))
+            action = agent.act(torch.unsqueeze(padded_world,0)).item()
+            print(action)
         state_prime, reward, done, _ = env.step(action)
         env.render()
         new_stacked_version = torch.cat((torch.cat((torch.unsqueeze(torch.from_numpy(state_prime['glyphs']),0),torch.unsqueeze(torch.from_numpy(state_prime['colors']),0))),torch.unsqueeze(torch.from_numpy(state_prime['chars']),0)))
@@ -156,8 +157,6 @@ if __name__ == '__main__':
             episode_rewards.append(total_reward)
             total_reward = 0
             env.reset()
-        #print(t)
-
         if (
                 t > hyper_params["learning-starts"]
                 and t % hyper_params["learning-freq"] == 0
